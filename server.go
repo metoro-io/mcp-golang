764baab4b121d268d04f37d58dc1922145edfc14
Niclas Jern <niclas@walkbase.com>
2025-04-20 21:12:53 +0300

    ServerOptions: Add WithInstructions

--- server.go
+++ server.go
@@ -163,6 +163,12 @@ func WithName(name string) ServerOptions {
 	}
 }
 
+func WithInstructions(instructions string) ServerOptions {
+	return func(s *Server) {
+		s.serverInstructions = &instructions
+	}
+}
+
 func WithVersion(version string) ServerOptions {
 	return func(s *Server) {
 		s.serverVersion = version
